005b00135a41bbdf201f83d3f3074323e87096fb
andejx <andejx@gmail.com>
2013-02-05 02:06:45 +1100

    lots of config generation and resetting
    still have to do the config changes being applied while the program is
    running though. I'm thinking I'll save settings when the tab changes,
    rather than when any variable changes.

--- TrapLaunch/Main.Designer.vb
+++ TrapLaunch/Main.Designer.vb
@@ -24,20 +24,33 @@ Partial Class Main
     Private Sub InitializeComponent()
         Me.buttonupdate = New System.Windows.Forms.Button()
         Me.progbarupdate = New System.Windows.Forms.ProgressBar()
-        Me.labelversion = New System.Windows.Forms.Label()
+        Me.labelmcversion = New System.Windows.Forms.Label()
         Me.buttonplay = New System.Windows.Forms.Button()
         Me.numericupdownmaxram = New System.Windows.Forms.NumericUpDown()
         Me.labelrammax = New System.Windows.Forms.Label()
         Me.labelminram = New System.Windows.Forms.Label()
         Me.NumericUpDownminram = New System.Windows.Forms.NumericUpDown()
-        Me.testbutton = New System.Windows.Forms.Button()
+        Me.maintab = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.Settings = New System.Windows.Forms.TabPage()
+        Me.buttondefaultsettings = New System.Windows.Forms.Button()
+        Me.labelftpusername = New System.Windows.Forms.Label()
+        Me.labelftppassword = New System.Windows.Forms.Label()
+        Me.labelftpserver = New System.Windows.Forms.Label()
+        Me.textboxftppassword = New System.Windows.Forms.TextBox()
+        Me.TextBoxftpusername = New System.Windows.Forms.TextBox()
+        Me.textboxftpaddress = New System.Windows.Forms.TextBox()
+        Me.labellauncherversion = New System.Windows.Forms.Label()
         CType(Me.numericupdownmaxram, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.NumericUpDownminram, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.maintab.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.Settings.SuspendLayout()
         Me.SuspendLayout()
         '
         'buttonupdate
         '
-        Me.buttonupdate.Location = New System.Drawing.Point(318, 213)
+        Me.buttonupdate.Location = New System.Drawing.Point(292, 6)
         Me.buttonupdate.Name = "buttonupdate"
         Me.buttonupdate.Size = New System.Drawing.Size(199, 36)
         Me.buttonupdate.TabIndex = 0
@@ -46,25 +59,25 @@ Partial Class Main
         '
         'progbarupdate
         '
-        Me.progbarupdate.Location = New System.Drawing.Point(318, 184)
+        Me.progbarupdate.Location = New System.Drawing.Point(292, 48)
         Me.progbarupdate.Name = "progbarupdate"
         Me.progbarupdate.Size = New System.Drawing.Size(199, 23)
         Me.progbarupdate.TabIndex = 1
         Me.progbarupdate.Visible = False
         '
-        'labelversion
+        'labelmcversion
         '
-        Me.labelversion.AutoSize = True
-        Me.labelversion.Location = New System.Drawing.Point(14, 9)
-        Me.labelversion.Name = "labelversion"
-        Me.labelversion.Size = New System.Drawing.Size(88, 13)
-        Me.labelversion.TabIndex = 2
-        Me.labelversion.Text = "Current Version : "
+        Me.labelmcversion.AutoSize = True
+        Me.labelmcversion.Location = New System.Drawing.Point(310, 88)
+        Me.labelmcversion.Name = "labelmcversion"
+        Me.labelmcversion.Size = New System.Drawing.Size(107, 13)
+        Me.labelmcversion.TabIndex = 2
+        Me.labelmcversion.Text = "Current MC Version : "
         '
         'buttonplay
         '
         Me.buttonplay.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.buttonplay.Location = New System.Drawing.Point(318, 83)
+        Me.buttonplay.Location = New System.Drawing.Point(292, 113)
         Me.buttonplay.Name = "buttonplay"
         Me.buttonplay.Size = New System.Drawing.Size(199, 95)
         Me.buttonplay.TabIndex = 3
@@ -74,7 +87,7 @@ Partial Class Main
         'numericupdownmaxram
         '
         Me.numericupdownmaxram.Increment = New Decimal(New Integer() {1024, 0, 0, 0})
-        Me.numericupdownmaxram.Location = New System.Drawing.Point(88, 62)
+        Me.numericupdownmaxram.Location = New System.Drawing.Point(100, 42)
         Me.numericupdownmaxram.Maximum = New Decimal(New Integer() {8192, 0, 0, 0})
         Me.numericupdownmaxram.Minimum = New Decimal(New Integer() {1024, 0, 0, 0})
         Me.numericupdownmaxram.Name = "numericupdownmaxram"
@@ -86,17 +99,17 @@ Partial Class Main
         '
         Me.labelrammax.AutoSize = True
         Me.labelrammax.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.labelrammax.Location = New System.Drawing.Point(14, 62)
+        Me.labelrammax.Location = New System.Drawing.Point(15, 42)
         Me.labelrammax.Name = "labelrammax"
-        Me.labelrammax.Size = New System.Drawing.Size(68, 16)
+        Me.labelrammax.Size = New System.Drawing.Size(72, 16)
         Me.labelrammax.TabIndex = 5
-        Me.labelrammax.Text = "MaxRam"
+        Me.labelrammax.Text = "Max Ram"
         '
         'labelminram
         '
         Me.labelminram.AutoSize = True
         Me.labelminram.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.labelminram.Location = New System.Drawing.Point(14, 36)
+        Me.labelminram.Location = New System.Drawing.Point(15, 16)
         Me.labelminram.Name = "labelminram"
         Me.labelminram.Size = New System.Drawing.Size(68, 16)
         Me.labelminram.TabIndex = 7
@@ -105,7 +118,7 @@ Partial Class Main
         'NumericUpDownminram
         '
         Me.NumericUpDownminram.Increment = New Decimal(New Integer() {512, 0, 0, 0})
-        Me.NumericUpDownminram.Location = New System.Drawing.Point(88, 36)
+        Me.NumericUpDownminram.Location = New System.Drawing.Point(100, 16)
         Me.NumericUpDownminram.Maximum = New Decimal(New Integer() {8192, 0, 0, 0})
         Me.NumericUpDownminram.Minimum = New Decimal(New Integer() {512, 0, 0, 0})
         Me.NumericUpDownminram.Name = "NumericUpDownminram"
@@ -113,45 +126,164 @@ Partial Class Main
         Me.NumericUpDownminram.TabIndex = 8
         Me.NumericUpDownminram.Value = New Decimal(New Integer() {512, 0, 0, 0})
         '
-        'testbutton
+        'maintab
         '
-        Me.testbutton.Location = New System.Drawing.Point(12, 88)
-        Me.testbutton.Name = "testbutton"
-        Me.testbutton.Size = New System.Drawing.Size(75, 23)
-        Me.testbutton.TabIndex = 9
-        Me.testbutton.Text = "Test"
-        Me.testbutton.UseVisualStyleBackColor = True
+        Me.maintab.Controls.Add(Me.TabPage1)
+        Me.maintab.Controls.Add(Me.Settings)
+        Me.maintab.Location = New System.Drawing.Point(12, 12)
+        Me.maintab.Name = "maintab"
+        Me.maintab.SelectedIndex = 0
+        Me.maintab.Size = New System.Drawing.Size(505, 240)
+        Me.maintab.TabIndex = 10
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.labelmcversion)
+        Me.TabPage1.Controls.Add(Me.buttonplay)
+        Me.TabPage1.Controls.Add(Me.progbarupdate)
+        Me.TabPage1.Controls.Add(Me.buttonupdate)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(497, 214)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Main"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'Settings
+        '
+        Me.Settings.Controls.Add(Me.labellauncherversion)
+        Me.Settings.Controls.Add(Me.buttondefaultsettings)
+        Me.Settings.Controls.Add(Me.labelftpusername)
+        Me.Settings.Controls.Add(Me.labelftppassword)
+        Me.Settings.Controls.Add(Me.labelftpserver)
+        Me.Settings.Controls.Add(Me.textboxftppassword)
+        Me.Settings.Controls.Add(Me.TextBoxftpusername)
+        Me.Settings.Controls.Add(Me.textboxftpaddress)
+        Me.Settings.Controls.Add(Me.labelminram)
+        Me.Settings.Controls.Add(Me.NumericUpDownminram)
+        Me.Settings.Controls.Add(Me.numericupdownmaxram)
+        Me.Settings.Controls.Add(Me.labelrammax)
+        Me.Settings.Location = New System.Drawing.Point(4, 22)
+        Me.Settings.Name = "Settings"
+        Me.Settings.Padding = New System.Windows.Forms.Padding(3)
+        Me.Settings.Size = New System.Drawing.Size(497, 214)
+        Me.Settings.TabIndex = 1
+        Me.Settings.Text = "Settings"
+        Me.Settings.UseVisualStyleBackColor = True
+        '
+        'buttondefaultsettings
+        '
+        Me.buttondefaultsettings.Location = New System.Drawing.Point(6, 186)
+        Me.buttondefaultsettings.Name = "buttondefaultsettings"
+        Me.buttondefaultsettings.Size = New System.Drawing.Size(77, 22)
+        Me.buttondefaultsettings.TabIndex = 15
+        Me.buttondefaultsettings.Text = "Defaults"
+        Me.buttondefaultsettings.UseVisualStyleBackColor = True
+        '
+        'labelftpusername
+        '
+        Me.labelftpusername.AutoSize = True
+        Me.labelftpusername.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.labelftpusername.Location = New System.Drawing.Point(15, 95)
+        Me.labelftpusername.Name = "labelftpusername"
+        Me.labelftpusername.Size = New System.Drawing.Size(108, 16)
+        Me.labelftpusername.TabIndex = 14
+        Me.labelftpusername.Text = "Ftp UserName"
+        '
+        'labelftppassword
+        '
+        Me.labelftppassword.AutoSize = True
+        Me.labelftppassword.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.labelftppassword.Location = New System.Drawing.Point(15, 121)
+        Me.labelftppassword.Name = "labelftppassword"
+        Me.labelftppassword.Size = New System.Drawing.Size(102, 16)
+        Me.labelftppassword.TabIndex = 13
+        Me.labelftppassword.Text = "Ftp Password"
+        '
+        'labelftpserver
+        '
+        Me.labelftpserver.AutoSize = True
+        Me.labelftpserver.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.labelftpserver.Location = New System.Drawing.Point(15, 68)
+        Me.labelftpserver.Name = "labelftpserver"
+        Me.labelftpserver.Size = New System.Drawing.Size(80, 16)
+        Me.labelftpserver.TabIndex = 12
+        Me.labelftpserver.Text = "Ftp Server"
+        '
+        'textboxftppassword
+        '
+        Me.textboxftppassword.Location = New System.Drawing.Point(129, 120)
+        Me.textboxftppassword.Name = "textboxftppassword"
+        Me.textboxftppassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.textboxftppassword.Size = New System.Drawing.Size(240, 20)
+        Me.textboxftppassword.TabIndex = 11
+        Me.textboxftppassword.Text = "test123"
+        '
+        'TextBoxftpusername
+        '
+        Me.TextBoxftpusername.Location = New System.Drawing.Point(129, 94)
+        Me.TextBoxftpusername.Name = "TextBoxftpusername"
+        Me.TextBoxftpusername.Size = New System.Drawing.Size(240, 20)
+        Me.TextBoxftpusername.TabIndex = 10
+        Me.TextBoxftpusername.Text = "test"
+        '
+        'textboxftpaddress
+        '
+        Me.textboxftpaddress.Location = New System.Drawing.Point(129, 68)
+        Me.textboxftpaddress.Name = "textboxftpaddress"
+        Me.textboxftpaddress.Size = New System.Drawing.Size(240, 20)
+        Me.textboxftpaddress.TabIndex = 9
+        Me.textboxftpaddress.Text = "ftp://localhost"
+        '
+        'labellauncherversion
+        '
+        Me.labellauncherversion.AutoSize = True
+        Me.labellauncherversion.Location = New System.Drawing.Point(89, 191)
+        Me.labellauncherversion.Name = "labellauncherversion"
+        Me.labellauncherversion.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.labellauncherversion.Size = New System.Drawing.Size(102, 13)
+        Me.labellauncherversion.TabIndex = 16
+        Me.labellauncherversion.Text = "Launcher Version :  "
         '
         'Main
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(529, 261)
-        Me.Controls.Add(Me.testbutton)
-        Me.Controls.Add(Me.NumericUpDownminram)
-        Me.Controls.Add(Me.labelminram)
-        Me.Controls.Add(Me.labelrammax)
-        Me.Controls.Add(Me.numericupdownmaxram)
-        Me.Controls.Add(Me.buttonplay)
-        Me.Controls.Add(Me.labelversion)
-        Me.Controls.Add(Me.progbarupdate)
-        Me.Controls.Add(Me.buttonupdate)
+        Me.Controls.Add(Me.maintab)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Name = "Main"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "TrapLaunch0.1"
         CType(Me.numericupdownmaxram, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.NumericUpDownminram, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.maintab.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.Settings.ResumeLayout(False)
+        Me.Settings.PerformLayout()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
     Friend WithEvents buttonupdate As System.Windows.Forms.Button
     Friend WithEvents progbarupdate As System.Windows.Forms.ProgressBar
-    Friend WithEvents labelversion As System.Windows.Forms.Label
+    Friend WithEvents labelmcversion As System.Windows.Forms.Label
     Friend WithEvents buttonplay As System.Windows.Forms.Button
     Friend WithEvents numericupdownmaxram As System.Windows.Forms.NumericUpDown
     Friend WithEvents labelrammax As System.Windows.Forms.Label
     Friend WithEvents labelminram As System.Windows.Forms.Label
     Friend WithEvents NumericUpDownminram As System.Windows.Forms.NumericUpDown
-    Friend WithEvents testbutton As System.Windows.Forms.Button
+    Friend WithEvents maintab As System.Windows.Forms.TabControl
+    Friend WithEvents TabPage1 As System.Windows.Forms.TabPage
+    Friend WithEvents Settings As System.Windows.Forms.TabPage
+    Friend WithEvents labelftpusername As System.Windows.Forms.Label
+    Friend WithEvents labelftppassword As System.Windows.Forms.Label
+    Friend WithEvents labelftpserver As System.Windows.Forms.Label
+    Friend WithEvents textboxftppassword As System.Windows.Forms.TextBox
+    Friend WithEvents TextBoxftpusername As System.Windows.Forms.TextBox
+    Friend WithEvents textboxftpaddress As System.Windows.Forms.TextBox
+    Friend WithEvents buttondefaultsettings As System.Windows.Forms.Button
+    Friend WithEvents labellauncherversion As System.Windows.Forms.Label
 
 End Class
--- TrapLaunch/Main.vb
+++ TrapLaunch/Main.vb
@@ -1,11 +1,51 @@
-﻿Public Class Main
+﻿Imports System.IO
+
+Public Class Main
     'sets up variables
     Dim javaminram As Decimal
     Dim javamaxram As Decimal
     Dim javaargs As String
+    Dim remoteFile As String
+    Dim localFile As String
+    Dim host As String
+    Dim username As String
+    Dim password As String
+
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim checkconfig As Boolean
+        'checks if launcher config exists, if it doesn't it calls configgenerate() and makes one.
+        checkconfig = File.Exists("./config.txt")
+        If checkconfig = False Then
+            configgenerate()
+        Else
+            'if a config does exist it reads the values and sets the settings!
+            configread()
+        End If
+    End Sub
 
+    Private Sub configgenerate()
+        'writes up a default config.txt with empty lines for things unknown
+        Using write1 As StreamWriter = File.AppendText("config.txt")
+            write1.WriteLine("TrapCraft0.1") 'launcher version
+            write1.WriteLine(" ") 'minecraft client version
+            write1.WriteLine("512") 'minram
+            write1.WriteLine("1024") 'maxram
+            write1.WriteLine("ftp://localhost") 'ftphost
+            write1.WriteLine("test") 'ftpusername
+            write1.WriteLine("test123") 'ftppassword
+            write1.Close()
+        End Using
+    End Sub
+
+    Private Sub configread()
+        Dim configread() As String
+        configread = File.ReadAllLines("./config.txt")
+        NumericUpDownminram.Value = configread(2)
+        numericupdownmaxram.Value = configread(3)
+        textboxftpaddress.Text = configread(4)
+        TextBoxftpusername.Text = configread(5)
+        textboxftppassword.Text = configread(6)
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles buttonupdate.Click
@@ -14,7 +54,9 @@
         progbarupdate.Value = 1
         Try
             'it then tries to connect to a server and download a current modlist
-            My.Computer.Network.DownloadFile("http://test", ".\modlist.txt", "", "", False, 100, True)
+            remoteFile = "/modlist.txt"
+            localFile = "./modlist.txt"
+            ftpdownload()
             'if it succeeds then it sets progress bar a little further along, then it converts the contents of the,
             'downloaded modlist to a string, and then to a msgbox for debug purposes, this section will eventually, 
             'do the mod version checking 
@@ -28,7 +70,6 @@
             MsgBox("Couldn't Retrieve Latest Client Info")
             progbarupdate.Value = 0
         End Try
-
     End Sub
 
     Private Sub buttonplay_Click(sender As Object, e As EventArgs) Handles buttonplay.Click
@@ -51,12 +92,76 @@
     End Sub
 
     Private Sub numericupdownmaxram_ValueChanged(sender As Object, e As EventArgs) Handles numericupdownmaxram.ValueChanged
+        'sets max ram whenever it's changed
         javamaxram = numericupdownmaxram.Value
     End Sub
 
-    Private Sub testbutton_Click(sender As Object, e As EventArgs) Handles testbutton.Click
-        'lil test button to output the settings in a msgbox so i can check its working
-        javaargs = ("-Xms" & javaminram & "m -Xmx" & javamaxram & "m -jar "".\minecraft.bat""")
-        MsgBox(javaargs)
+
+    Private Sub ftpdownload()
+        'does the ftp downloadin based on w/e the program has 
+        'remoteFile and localFile strings set to. ie ".remote/modlist.txt" ,".local/modlist.txt"
+        host = textboxftpaddress.Text
+        username = TextBoxftpusername.Text
+        password = textboxftppassword.Text
+
+        Dim URI As String = host & remoteFile
+        Dim ftp As System.Net.FtpWebRequest = _
+            CType(Net.FtpWebRequest.Create(URI), Net.FtpWebRequest)
+
+        ftp.Credentials = New  _
+    System.Net.NetworkCredential(username, password)
+        ftp.KeepAlive = True
+        ftp.UseBinary = True
+        ftp.Method = System.Net.WebRequestMethods.Ftp.DownloadFile
+        Using response As System.Net.FtpWebResponse = _
+      CType(ftp.GetResponse, System.Net.FtpWebResponse)
+            Using responseStream As IO.Stream = response.GetResponseStream
+                'loop to read & write to file
+                Using fs As New IO.FileStream(localFile, IO.FileMode.Create)
+                    Dim buffer(2047) As Byte
+                    Dim read As Integer = 0
+                    Do
+                        read = responseStream.Read(buffer, 0, buffer.Length)
+                        fs.Write(buffer, 0, read)
+                    Loop Until read = 0 'see Note(1)
+                    responseStream.Close()
+                    fs.Flush()
+                    fs.Close()
+                End Using
+                responseStream.Close()
+            End Using
+            response.Close()
+        End Using
+        remoteFile = ""
+        localFile = ""
     End Sub
+
+
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        'Makes an are you sure you want to close window.
+        Dim result As DialogResult
+        result = MessageBox.Show("Are you sure?", "Close program?", MessageBoxButtons.YesNo)
+        If result = Windows.Forms.DialogResult.Yes Then
+            e.Cancel = False
+        Else
+            e.Cancel = True
+        End If
+    End Sub
+
+    Private Sub buttondefaultsettings_Click(sender As Object, e As EventArgs) Handles buttondefaultsettings.Click
+        'resets all config options by regenerating the config file then rereading the config.
+        File.Delete("./config.txt")
+        Using write1 As StreamWriter = File.AppendText("config.txt")
+            write1.WriteLine("TrapCraft0.1")
+            write1.WriteLine(" ")
+            write1.WriteLine("512") 
+            write1.WriteLine("1024")
+            write1.WriteLine("ftp://localhost")
+            write1.WriteLine("test")
+            write1.WriteLine("test123")
+            write1.Close()
+        End Using
+        configread()
+    End Sub
+
 End Class
